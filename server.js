0bdd098f95b7fc592ad5e896889b70f2f66fe3f7
Armando <xxnowonxx@gmail.com>
2018-09-23 15:09:38 -0700

    heroku db updates

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/sportScraper";
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true});
 
 // Routes
 
